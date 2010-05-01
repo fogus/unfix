6d4316d58519189bd79737d1b989aca95b80deae
fogus <mefogus@gmail.com>
2010-04-30 20:57:53 -0400

    Added the version from the 2nd MEAP addtion.  This was taken out of the book due to its polarizing nature, but we still felt it could be useful to others.  So here it is.

---
+++ src/joy/unfix/infix.clj
@@ -0,0 +1,16 @@
+(ns joy.unfix)
+
+(def AND #(and %1 %2))
+(def *rank* (zipmap [- + * / AND =] (iterate inc 1))) ;; Define order of precedence
+    
+(defn infix* [[a b & [c d e & more]]]
+  (cond
+   (vector? a) (recur (list* (infix* a) b c d e more)) ;; Vector on the LHS
+   (vector? c) (recur (list* a b (infix* c) d e more)) ;; Vector on the RHS
+   (ifn? b)    (if (and d (< (rank b 0) (rank d 0)))
+                 (recur (list a b (infix* (list* c d e more)))) ;; Left-association
+                 (recur (list* (b a c) d e more))) ;; Right-association
+   :else a)) ;; Return the left-most element
+    
+(defn infix [& args]
+  (infix* args))
