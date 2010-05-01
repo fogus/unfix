b0016bf948c4f3c0a33d3788c00c82004cb3a3b2
fogus <mefogus@gmail.com>
2010-04-30 22:08:23 -0400

    Started adding tests for the infix impl

---
+++ test/joy/unfix/infix_tests.clj
@@ -0,0 +1,22 @@
+(ns joy.unfix.infix-tests
+  (:require [clojure.test :as test])
+  (:use joy.unfix.infix))
+
+(defn- all-eq? [a b c] 
+  (infix 
+    a = b && 
+    b = c && 
+    a = c))
+
+(test/deftest simple-infix
+  (test/is (= 3
+              (infix 21 / [1 + 2 * 3])))
+  (test/is (= 9
+              (infix [1 + 2] * 3)))
+  (test/is (= 7
+              (infix 1 + 2 * 3))))
+
+(test/deftest compound-infix
+  (test/is (all-eq? 3 3 3))
+  (test/is (not (all-eq? 1 2 3))))
+
