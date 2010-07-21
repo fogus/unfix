3c40e112e853002b459be11643843e3f9687fa9c
fogus <mefogus@gmail.com>
2010-07-21 14:41:16 -0400

    Updated README

---
+++ README.md
@@ -0,0 +1,23 @@
+Unfix is a simple library providing infix and postfix notation in Clojure.
+
+Usage:
+
+    (infix [1 + 2] * 3)
+    ;=> 9
+    
+    (postfix 3 2 1 + *)
+    ;=> [9]
+
+via. Leiningen:
+
+    [unfix "1.0"]
+
+via Maven:
+
+    <dependency>
+      <groupId>unfix</groupId>
+      <artifactId>unfix</artifactId>
+      <version>1.0</version>
+    </dependency>
+
+More information can be found at the [official unfix site](http://fogus.me/fun/unfix)
--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject unfix "0.0.1"
-  :description "A library profiding infix and postfix notation in Clojure."
+(defproject unfix "1.0"
+  :description "A library providing infix and postfix notation in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.1.0"]
-                     [jline "0.9.94"]])
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
