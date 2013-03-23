23273e9156ff2c889d6b5794e7fdd7902f0c2344
fogus <mefogus@gmail.com>
2013-03-22 22:41:12 -0400

    Working off of symbols and macrology

--- src/joy/unfix/infix.clj
+++ src/joy/unfix/infix.clj
@@ -6,16 +6,18 @@
 (def ^:dynamic *ops* '[- + * / < > && || =])
 (def rank (zipmap *ops* (iterate inc 1)))
 (def op? rank)
-    
+(def ^:dynamic _ clojure.core/resolve)
+
 (defn- infix* 
-  [[a b & [c d e & more]]]
+  [[a b & [c d e & more] :as v]]
   (cond
    (vector? a) (recur (list* (infix* a) b c d e more))
    (vector? c) (recur (list* a b (infix* c) d e more))
    (op? b)     (if (and d (< (rank b 0) (rank d 0)))
                  (recur (list a b (infix* (list* c d e more))))
-                 (recur (list* (list b a c) d e more)))
+                 (recur (list* (list (_ b) a c) d e more)))
    :else a))
     
-(defn infix [& args]
+(defmacro infix [& args]
   (infix* args))
+
