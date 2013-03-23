8b0049ef9facf3eb0c25c38d11796ede9dbedd45
fogus <mefogus@gmail.com>
2013-03-22 22:07:55 -0400

    Working through symbol-based infix

--- src/joy/unfix/infix.clj
+++ src/joy/unfix/infix.clj
@@ -3,16 +3,18 @@
 (def && #(and % %2))
 (def || #(or  % %2))
 
-(def *rank* (zipmap [- + * / < > && || =] (iterate inc 1)))
+(def ^:dynamic *ops* '[- + * / < > && || =])
+(def rank (zipmap *ops* (iterate inc 1)))
+(def op? rank)
     
 (defn- infix* 
   [[a b & [c d e & more]]]
   (cond
    (vector? a) (recur (list* (infix* a) b c d e more))
    (vector? c) (recur (list* a b (infix* c) d e more))
-   (ifn? b)    (if (and d (< (*rank* b 0) (*rank* d 0)))
+   (op? b)     (if (and d (< (rank b 0) (rank d 0)))
                  (recur (list a b (infix* (list* c d e more))))
-                 (recur (list* (b a c) d e more)))
+                 (recur (list* (list b a c) d e more)))
    :else a))
     
 (defn infix [& args]
