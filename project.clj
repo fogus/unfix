7953b070003a2cb19f73f31ce2120f04b2474ed6
fogus <mefogus@gmail.com>
2013-03-22 21:56:58 -0400

    Upgraded to clj 1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject unfix "1.0"
+(defproject unfix "1.5"
   :description "A library providing infix and postfix notation in Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
