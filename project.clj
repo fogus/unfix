b931e198f5b6ac5bca3c7b5b752f94df9ce7d9a2
Fogus <mefogus@gmail.com>
2013-04-07 09:46:51 -0300

    Upgrade Clojure version to v1.5.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject unfix "1.5"
   :description "A library providing infix and postfix notation in Clojure."
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
