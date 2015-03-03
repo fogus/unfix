c23254dc0c9500fb099896ccac705306cd609bdf
Paul DeLong <Paul.DeLong@gmail.com>
2015-03-03 18:56:49 -0500

    [pdelong42/my-changes] version bump
    got the tests working, so it's a good time to release;
    bumping-up the major version number, since the behaviour is changing drastically (semver.org);

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.clojars.pdelong/unfix "2.0"
+(defproject org.clojars.pdelong/unfix "3.0"
   :description "A library providing infix and postfix notation in Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]])
